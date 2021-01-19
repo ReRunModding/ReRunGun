56ffedde6bbd822dd604e1eba0fe0ef5d5ec0ac9
unknown <marijnkneppers@gmail.com>
2021-01-19 14:21:34 +0100

    initial commit

---
+++ ReRunGun/Main.cs
@@ -0,0 +1,39 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+namespace ReRunGun
+{
+    public class Main : MelonMod
+    {
+		bool gunActive = false;
+
+		public override void OnUpdate()
+		{
+			if (Input.GetKeyDown(KeyCode.G))
+			{
+				if (gunActive)
+				{
+					gunActive = false;
+				}
+				else
+				{
+					gunActive = true;
+				}
+			}
+
+			GameObject player = GameObject.Find("Player");
+			if (player != null)
+			{
+				if (gunActive)
+				{
+					player.GetComponent<PlayerPowers>().gun = true;
+				}
+				else
+				{
+					player.GetComponent<PlayerPowers>().gun = false;
+				}
+			}
+		}
+
+	}
+}
---
+++ ReRunGun/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ReRunGun")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReRunGun")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: MelonInfo(typeof(ReRunGun.Main), "ReRunGun", "1.0.0", "Mandar1jn")]
+[assembly: MelonGame("Dani", "RERUN")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c4024690-4281-47c4-ab1a-b79f065aece5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
